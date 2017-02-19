5f5c50ac8204dacc1fff6d83593f67008b990e6f
Agaba Kabunga <Agaba Kabunga>
2017-02-19 15:28:15 -0600

    create array

--- app/toBuyController.js
+++ app/toBuyController.js
@@ -7,7 +7,13 @@
 
      function toBuyController() {
           var toBuyItem = this;
-
+             vm.arrayItems = [
+                { name: "cookies", quantity: 10 },
+                { name: "loaf of bread", quantity: 1 },
+                { name: "Gallon of milk", quantity: 1},
+                { name: "Orange", quantity: 8 },
+                { name: "cookies", quantity: 10 }
+            ];
           toBuyItem.title = "To Buy Item";
      };
 }());
