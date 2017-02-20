2c49b64d6d34f50474ae678fbf39b0229f0d21d9
Agaba Kabunga <Agaba Kabunga>
2017-02-20 08:14:44 -0600

    testing the function

--- app/toBuyController.js
+++ app/toBuyController.js
@@ -10,5 +10,8 @@
              toBuyItem.arrayItems = shoppingListCheckOffService.getItems();
           toBuyItem.title = "Items List";
           toBuyItem.remove = shoppingListCheckOffService.removeItem();
+          toBuyItem.check = function(){
+            console.log('Its been log');
+          }
      };
 }());
