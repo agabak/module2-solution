6bc5e025ec92f7ca6688f9a5fc40d1ceb983eb52
Agaba Kabunga <Agaba Kabunga>
2017-02-20 08:20:21 -0600

    add list

--- app/toBuyController.js
+++ app/toBuyController.js
@@ -10,8 +10,8 @@
              toBuyItem.arrayItems = shoppingListCheckOffService.getItems();
           toBuyItem.title = "Items List";
           toBuyItem.remove = shoppingListCheckOffService.removeItem();
-          toBuyItem.check = function(){
-            console.log('Its been log');
+          toBuyItem.check = function(value){
+            console.log('Its been log', value);
           }
      };
 }());
