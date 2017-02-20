b2672035c7aae074f26af50bb97f9d98e003a5ac
Agaba Kabunga <Agaba Kabunga>
2017-02-20 07:41:06 -0600

    add the service

--- app/service/shoppingListCheckOffService.js
+++ app/service/shoppingListCheckOffService.js
@@ -4,17 +4,15 @@
    angular.module('shoppingListCheckOff')
        .service('shoppingListCheckOffService',shoppingListCheckOffService);
 
+       var items = [
+          { name: "cookies", quantity: 10 },
+          { name: "loaf of bread", quantity: 1 },
+          { name: "Gallon of milk", quantity: 1},
+          { name: "Orange", quantity: 8 },
+          { name: "cookies", quantity: 10 }
+      ];
        function shoppingListCheckOffService() {
                var  service =  this;
-
-               var items = [
-                  { name: "cookies", quantity: 10 },
-                  { name: "loaf of bread", quantity: 1 },
-                  { name: "Gallon of milk", quantity: 1},
-                  { name: "Orange", quantity: 8 },
-                  { name: "cookies", quantity: 10 }
-              ];
-
               service.getItems = function () {
                   return items;
               };
